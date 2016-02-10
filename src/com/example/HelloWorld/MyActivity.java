c034de93136933a8b3aba2bd781f22726ec7f6d8
Katerina <fl-katerina.potyomkina@yandex.ru>
2016-02-11 01:17:24 +0300

    start

---
+++ src/com/example/HelloWorld/MyActivity.java
@@ -0,0 +1,30 @@
+package com.example.HelloWorld;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MyActivity extends Activity {
+    TextView mainText;
+    Button button;
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        mainText = (TextView)findViewById(R.id.mainText);
+
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainText.setText("Мир, привет!");
+            }
+        });
+    }
+}
